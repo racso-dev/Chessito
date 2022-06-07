bfe3db8f134194dd1e1323c78931ecde0fb7cc84
Oscar Rénier <oscar.renier@gmail.com>
2022-06-08 01:07:55 +0200

    [ADD]: Implementation of chess.js

--- public/scripts/board.js
+++ public/scripts/board.js
@@ -1,16 +1,76 @@
-Chessboard('board', {
+let board = null;
+let game = new Chess();
+let $status = $('#status');
+let $fen = $('#fen');
+let $pgn = $('#pgn');
+
+function onDragStart(source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false;
+
+  // only pick up pieces for the side to move
+  if (
+    (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+    (game.turn() === 'b' && piece.search(/^w/) !== -1)
+  ) {
+    return false;
+  }
+}
+
+function onDrop(source, target) {
+  // see if the move is legal
+  let move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q', // NOTE: always promote to a queen for example simplicity
+  });
+
+  // illegal move
+  if (move === null) return 'snapback';
+
+  updateStatus();
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd() {
+  board.position(game.fen());
+}
+
+function updateStatus() {
+  let status = '';
+  let moveColor = 'White';
+  if (game.turn() === 'b') {
+    moveColor = 'Black';
+  }
+  // checkmate?
+  if (game.in_checkmate()) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.';
+  }
+  // draw?
+  else if (game.in_draw()) {
+    status = 'Game over, drawn position';
+  }
+  // game still on
+  else {
+    status = moveColor + ' to move';
+    // check?
+    if (game.in_check()) {
+      status += ', ' + moveColor + ' is in check';
+    }
+  }
+  $status.html(status);
+  $fen.html(game.fen());
+  $pgn.html(game.pgn());
+}
+
+let config = {
   draggable: true,
   position: 'start',
-  onDragStart: () => {
-    console.log('onDragStart');
-  },
-  onDrop: () => {
-    console.log('onDrop');
-  },
-  onSnapEnd: () => {
-    console.log('onSnapEnd');
-  },
-  onMoveEnd: () => {
-    console.log('onMoveEnd');
-  },
-});
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
+};
+board = Chessboard('board', config);
+
+updateStatus();
